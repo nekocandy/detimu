8c25e2a5517378721329d91087e97c1d79ce9fac
Piyush <bhangalepiyush@gmail.com>
2023-12-30 23:51:20 +0530

    feat: add logout, and more design

--- components/sidebar.vue
+++ components/sidebar.vue
@@ -1,5 +1,24 @@
 <script setup lang="ts">
+import { logIn, unauthenticate } from '~/utils/flow/auth'
 
+const router = useRouter()
+
+async function handleUserAuth() {
+  if (userData.value?.addr)
+    unauthenticate()
+
+  else
+    logIn()
+}
+
+watch(userData, (newValue, oldValue) => {
+  if (newValue?.addr && !oldValue?.addr)
+    router.push('/explore')
+  else
+    router.push('/')
+
+  consola.withTag('[WALLET]').info('userData changed from', oldValue, 'to', newValue)
+})
 </script>
 
 <template>
@@ -13,9 +32,24 @@
     </div>
 
     <div w-full>
-      <button bg-indigo-400 w-full px-8 py-4>
-        <span v-if="userData?.addr">{{ userData?.addr }}</span>
-        <span v-else>Login</span>
+      <button
+        :class="{
+          'bg-indigo-400': userData?.addr,
+          'bg-indigo-600': !userData?.addr,
+        }"
+        w-full px-8 py-1 truncate @click="handleUserAuth"
+      >
+        <div v-if="userData?.addr" flex items-center gap-1>
+          <div text-4xl i-material-symbols-shield-person text-white />
+          <span truncate>
+            {{ userData?.addr }}
+          </span>
+
+          <div i-material-symbols-logout text-3xl text-red-700 />
+        </div>
+        <div v-else>
+          Login
+        </div>
       </button>
     </div>
   </div>
